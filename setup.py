ac0e6638e0bea46bf8c6576d2e606c6e7b630f33
Andre B <andrebassbusiness@gmail.com>
2021-09-03 15:39:51 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='intuition',
+      version='v1.0',
+      description='Software designed to identify and monitor social/historical cues for short term stock movement',
+      author='Anthony Federico',
+      author_email='andrebassbusiness@gmail.com',
+      url='https://github.com/theandrebass/intuition',
+      packages=['intuition']
+     )
